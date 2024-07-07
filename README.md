805340fe6d9024f2091d55df2504d51fa22b5fec
shiv3 <tshibaza@mercari.com>
2024-07-07 15:14:55 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# OCPP ChargePoint Simulator React
+
+rewrited with react
+
+https://shiv3.github.io/OCPP-ChargePoint-Simulator-React
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// App.tsx
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar.tsx";
+import ChargePoint from "./components/ChargePoint.tsx";
+import Connector from "./components/Connector.tsx";
+import Settings from "./components/Settings.tsx";
+import Logger from "./components/Logger.tsx";
+
+const App: React.FC = () => {
+  return (
+    <Router basename={import.meta.env.VITE_BASE_URL}>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar/>
+        <div className="container mx-auto my-auto p-4">
+          <Routes>
+            <Route path="/" element={<ChargePoint/>}/>
+            <Route path="/settings" element={<Settings/>}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
